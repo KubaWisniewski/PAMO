2ff073146c35c939939457fa0e48e14c53e21f5f
DESKTOP-9MD9SOV\Kuba <kubaw1996@gmail.com>
2019-10-20 11:12:08 +0200

    Rename

--- BmiCalculator/app/src/androidTest/java/com/example/bmicalculator/ExampleInstrumentedTest.java
+++ BmiCalculator/app/src/androidTest/java/com/example/bmicalculator/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapp;
+package com.example.bmicalculator;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
--- BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,12 +1,10 @@
-package com.example.myapp;
+package com.example.bmicalculator;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import java.text.DecimalFormat;
 
 public class MainActivity extends Activity {
 
